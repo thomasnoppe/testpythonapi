5091763f9bd041276d1d1374324451075e704e2b
thomasnoppe <49635320+thomasnoppe@users.noreply.github.com>
2023-08-30 18:58:39 +0000

    app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
- 
+from flask import Flask
+
 app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return render_template('index.html', message="Hello World..!!")
+
+@app.route('/whoami', methods=['GET'])
+def whoami():
+    return "thomas"
  
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
