807cd6129770beb6faa9a2cae4486ebfb73703c4
thomasnoppe <49635320+thomasnoppe@users.noreply.github.com>
2023-08-30 18:51:52 +0000

    code stuff

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM registry.access.redhat.com/ubi8/python-39:latest
+ 
+WORKDIR /deployment
+ 
+COPY app.py /deployment
+COPY templates/* /deployment/templates/
+COPY requirements.txt /deployment
+ 
+RUN pip3 install -r requirements.txt
+ 
+EXPOSE 5000
+ 
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
-
-@app.route('/whoami', methods=['GET'])
-def whoami():
-    return "thomas"
-
+ 
+@app.route("/")
+def index():
+    return render_template('index.html', message="Hello World..!!")
+ 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080, debug=True)
