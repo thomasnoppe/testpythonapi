8ae784822bbab4992f8def4ed5e36c6dd37c3960
thomasnoppe <49635320+thomasnoppe@users.noreply.github.com>
2023-08-30 19:45:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/whoami', methods=['GET'])
+def whoami():
+    return "thomas"
+
+if __name__ == '__main__':
+    app.run(debug=True)
